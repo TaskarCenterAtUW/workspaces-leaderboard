dbf97780791a67a7cd8b428446581ea3cf1a3f77
uwjohnbee <johnbee@uw.edu>
2025-09-16 14:45:42 -0400

    vite config changes, updated package lock

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,8 +5,10 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
 	build: {
+		outDir: 'dist', // Output directory for the production build
+   		sourcemap: true, // Generate sourcemaps for debugging
+   		minify: 'esbuild', // Minification strategy (terser or esbuild)
 		rollupOptions: {
-			external: ["vue"],
 			output: {
 				globals: {
 					vue: "Vue",
