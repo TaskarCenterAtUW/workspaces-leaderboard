0fdf096d9740b9fd1ba1b8562260f8cb5ee8d37d
uwjohnbee <johnbee@uw.edu>
2025-09-24 11:29:25 -0400

    db creds added via env vars, query tweakin' to include user's display_name, no more test data returned

--- backend/app.py
+++ backend/app.py
@@ -1,24 +1,28 @@
 from flask import Flask, request, jsonify
-import psycopg2
+import os, psycopg2
 
 app = Flask(__name__)
 
+# Access the environment variable
+db_host = os.environ.get("WS_OSM_DB_HOST")
+db_user = os.environ.get("WS_OSM_DB_USER")
+db_password = os.environ.get("WS_OSM_DB_PASS")
+
 def get_db_connection(sanitizedWorkspaceId):
     conn = psycopg2.connect(
-        host="localhost",
+        host=db_host,
         database="workspace" + "-" + sanitizedWorkspaceId,
-        user="postgres",
-        password="password"
+        user=db_user,
+        password=db_password
     )
     return conn
 
 @app.route('/api/leaderboard', methods=['GET'])
 def get_leaderboard():
-    '''
     workspaceId = request.args.get('filterWorkspace')
     time = request.args.get('filterTime')
 
-    interval = '100 years' // all time
+    interval = '100 years'
     match time:
         case 'week':
             interval = '1 week'
@@ -29,25 +33,18 @@ def get_leaderboard():
     conn = get_db_connection(sanitizedWorkspaceId)
     cursor = conn.cursor()
     query = '
-        SELECT c.user_id AS name, c.num_changes AS score 
+        SELECT u.display_name AS name, c.num_changes AS score 
         FROM changesets c 
+        INNER JOIN users u ON c.user_id = u.id
         WHERE c.closed_at >= NOW() - INTERVAL %s 
         GROUP BY c.user_id 
-        ORDER BY score DESC;
-    '
+        ORDER BY score DESC'
+    
     cursor.execute(query, (interval))
     rows = cursor.fetchall()
     cursor.close()
     conn.close()
     return jsonify([{'name': row[0], 'score': row[1]} for row in rows])
-    '''
-    return [
-    { "name": "Ostentatious Otters", "score": 1500 },
-    { "name": "Bouncy Birbs", "score": 1400 },
-    { "name": "Cute Capybaras", "score": 1350 },
-    { "name": "Delightful Ducks", "score": 1300 },
-    { "name": "Eager Elephants", "score": 1250 }
-    ]
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
