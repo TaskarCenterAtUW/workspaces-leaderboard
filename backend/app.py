0d24b8f48b313e74ebb12691d41f92b01978c64e
uwjohnbee <johnbee@uw.edu>
2025-09-25 13:06:22 -0400

    setting db schema, loading db name from env variable

--- backend/app.py
+++ backend/app.py
@@ -4,6 +4,7 @@ import html, os, psycopg2
 app = Flask(__name__)
 
 # Access the environment variable
+db_dbname = os.environ.get("WS_OSM_DB_NAME")
 db_host = os.environ.get("WS_OSM_DB_HOST")
 db_user = os.environ.get("WS_OSM_DB_USER")
 db_password = os.environ.get("WS_OSM_DB_PASS")
@@ -11,7 +12,7 @@ db_password = os.environ.get("WS_OSM_DB_PASS")
 def get_db_connection(sanitizedWorkspaceId):
     conn = psycopg2.connect(
         host=db_host,
-        database="workspace" + "-" + sanitizedWorkspaceId,
+        database=db_dbname,
         user=db_user,
         password=db_password
     )
@@ -32,13 +33,17 @@ def get_leaderboard():
     sanitizedWorkspaceId = html.escape(workspaceId)
     conn = get_db_connection(sanitizedWorkspaceId)
     cursor = conn.cursor()
+
+    schema_query = "SET search_path TO workspace-{sanitizedWorkspaceId}, public;"
+    cursor.execute(schema_query)
+
     query = """
         SELECT u.display_name AS name, c.num_changes AS score 
         FROM changesets c 
         INNER JOIN users u ON c.user_id = u.id
         WHERE c.closed_at >= NOW() - INTERVAL %s 
         GROUP BY c.user_id 
-        ORDER BY score DESC
+        ORDER BY score DESC;
     """
     
     cursor.execute(query, (interval))
